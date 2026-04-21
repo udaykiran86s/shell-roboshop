2479373d5eee4782a2cf71576266329abd504ee7
unknown <samanthulaudaykiran@gmail.com>
2026-04-21 18:14:25 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,20 +1,3 @@
-# #!/bin/bash
-# AMI_ID="ami-0220d79f3f480ecf5"
-# SG_ID="sg-08bc7d23acfac1667" #relace wish your sgid
-
-# for instance in $@
-# do 
-#     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-08bc7d23acfac1667 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query `Instances[0].InstanceId` --output text)
-#     #GET PRIVATE IP
-#     if [ $instance != "frontend" ];then
-#     IP=$(aws ec2 describe-instances --instance-ids i-03bfb25aafbf9edbb --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-#     else
-#        IP=$(aws ec2 describe-instances --instance-ids i-03bfb25aafbf9edbb --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-#     fi
-#         echo "$instance : $IP"
-# done
-
-
 #!/bin/bash
 
 AMI_ID="ami-0220d79f3f480ecf5"
@@ -52,3 +35,33 @@ do
 
     echo "$instance : $IP"
 done
+
+
+ZONE_ID=$(aws route53 create-hosted-zone \
+  --name udaykiran.site \
+  --caller-reference "$(date +%s)" \
+  --hosted-zone-config Comment="Public Hosted Zone",PrivateZone=false \
+  --query 'HostedZone.Id' \
+  --output text | cut -d'/' -f3)
+
+echo $ZONE_ID
+
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '{
+    "Changes": [
+      {
+        "Action": "CREATE",
+        "ResourceRecordSet": {
+          "Name": "'$RECORD_NAME'",
+          "Type": "A",
+          "TTL": 1,
+          "ResourceRecords": [
+            {
+              "Value": "'$IP'"
+            }
+          ]
+        }
+      }
+    ]
+  }'
