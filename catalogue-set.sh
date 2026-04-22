67005c378555caa235fef4256bb8854d08866ddd
unknown <samanthulaudaykiran@gmail.com>
2026-04-22 20:37:58 +0530

    messages

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -30,11 +30,11 @@ dnf module enable nodejs:20 -y  &>>$LOG_FILE
 dnf install nodejs -y &>>$LOG_FILE
 echo -e "Installing NodeJS 20 ... $G SUCCESS $N"
 
-id roboshop &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-else
+
+if id roboshop &>>$LOG_FILE; then
     echo -e "User already exist ... $Y SKIPPING $N"
+else
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
 fi
 
 mkdir -p /app
