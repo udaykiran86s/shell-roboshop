1a7d29c797d5e1f13909f238b591354d5f75798e
unknown <samanthulaudaykiran@gmail.com>
2026-04-22 14:14:38 +0530

    messages

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -49,9 +49,9 @@ systemctl enable catalogue &>>$LOG_FILE
 echo -e "Catalogue application setup ... $G SUCCESS $N"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongoshsfds -y &>>$LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh mongo.udaykiran.site --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
 else
