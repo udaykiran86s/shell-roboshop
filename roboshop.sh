14c3e577ded0b0a37da01983cc52ae2ebe74b9fc
unknown <samanthulaudaykiran@gmail.com>
2026-04-22 16:02:09 +0530

    messages

--- roboshop.sh
+++ roboshop.sh
@@ -67,26 +67,26 @@ do
 
 
 
-RECORD_NAME="${instance}.${DOMAIN_NAME}"
-aws route53 change-resource-record-sets \
-  --hosted-zone-id $ZONE_ID \
-  --change-batch '{
-    "Changes": [
-      {
-        "Action": "UPSERT",
-        "ResourceRecordSet": {
-          "Name": "'$RECORD_NAME'",
-          "Type": "A",
-          "TTL": 1,
-          "ResourceRecords": [
-            {
-              "Value": "'$IP'"
-            }
-          ]
-        }
-      }
-    ]
-  }'
+# RECORD_NAME="${instance}.${DOMAIN_NAME}"
+# aws route53 change-resource-record-sets \
+#   --hosted-zone-id $ZONE_ID \
+#   --change-batch '{
+#     "Changes": [
+#       {
+#         "Action": "UPSERT",
+#         "ResourceRecordSet": {
+#           "Name": "'$RECORD_NAME'",
+#           "Type": "A",
+#           "TTL": 1,
+#           "ResourceRecords": [
+#             {
+#               "Value": "'$IP'"
+#             }
+#           ]
+#         }
+#       }
+#     ]
+#   }'
 
   
 done
