bb901e87ea4e414d8d33357b8fff3dfc6e2eac78
unknown <samanthulaudaykiran@gmail.com>
2026-04-22 15:40:52 +0530

    messages

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -51,6 +51,7 @@ echo -e "Catalogue application setup ... $G SUCCESS $N"
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 
+
 INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
